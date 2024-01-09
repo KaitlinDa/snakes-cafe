6e1509d48b301c11c6f00787f597427a407f146a
KaitlinDa <Kaitlin.A.Davis1996@Gmail.com>
2024-01-08 22:21:44 -0600

    complete lab 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# Author
+
+Kaitlin Davis | January 2024
+
+# About
+
+I am creating a command line utility that will mimic the functionality of a point of sale restaurant system using basic Python tools and understanding.
+
+# Resources
+
+I used the help of some TA's as well as ChatGPT for this assignment. 
--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,3 +1,53 @@
-print("an intro message and the menu for the restaurant")
-input1 = input("give me your order")
-print(input1)
+def print_menu():
+    print("**************************************")
+    print("**    Welcome to the Snakes Cafe!   **")
+    print("**    Please see our menu below.    **")
+    print("**")
+    print("** To quit at any time, type \"quit\" **")
+    print("**************************************\n")
+
+    print("Appetizers")
+    print("----------")
+    print("Wings")
+    print("Cookies")
+    print("Spring Rolls\n")
+
+    print("Entrees")
+    print("-------")
+    print("Salmon")
+    print("Steak")
+    print("Meat Tornado")
+    print("A Literal Garden\n")
+
+    print("Desserts")
+    print("--------")
+    print("Ice Cream")
+    print("Cake")
+    print("Pie\n")
+
+    print("Drinks")
+    print("------")
+    print("Coffee")
+    print("Tea")
+    print("Unicorn Tears\n")
+
+    print("***********************************")
+    print("** What would you like to order? **")
+    print("***********************************")
+
+
+def main():
+    order_counts = {}
+    print_menu()
+
+    while True:
+        order = input("> ")
+
+        if order.lower() == "quit":
+            break
+        elif order:
+            order_counts[order] = order_counts.get(order, 0) + 1
+            print(f"\n** {order_counts[order]} order of {order} has been added to your meal **\n")
+
+if __name__ == "__main__":
+    main()
